4445ec47e31f81016e1d2dd887a7f07877f1fb64
Istvan Balazs Opra <opra.balazs@gmail.com>
2022-01-11 17:52:42 +0100

    Add a visual test for the Harris Detector
    Add script to download data for the visual test

--- harris/tests/harris_detector_test.py
+++ harris/tests/harris_detector_test.py
@@ -1,12 +1,18 @@
+from pathlib import Path
 import unittest
 
+import cv2.cv2
 import cv2.cv2 as cv
 import numpy as np
+
+import sys
+
+sys.path.append("/Users/bopra/code/structure_from_motion")
 
 import harris.harris_detector as harris
 
 
-class HarrisDetectorTest(unittest.TestCase):
+class TestHarrisDetector(unittest.TestCase):
     def test_cross_correlate_basic(self):
         input_image = np.ones((5, 10), dtype=float)
         kernel = np.ones((3, 3), dtype=float)
@@ -58,3 +64,42 @@ class HarrisDetectorTest(unittest.TestCase):
         corner_coordinates = harris.detect_harris_corners(image)
         for expected_corner, corner in zip(expected_corners, corner_coordinates):
             self.assertTrue(np.allclose(expected_corner, corner, atol=1.0))
+
+    def test_harris_visual(self):
+        test_image_path = Path(
+            "/Users/bopra/code/structure_from_motion/data/barcelona/DSCN8238.JPG"
+        )
+        self.assertTrue(test_image_path.is_file())
+
+        test_image = cv.imread(str(test_image_path))
+        downscale_factor = 8
+        smaller_size = (
+            int(test_image.shape[1] / downscale_factor),
+            int(test_image.shape[0] / downscale_factor),
+        )
+        test_image = cv.resize(
+            test_image, smaller_size, interpolation=cv2.INTER_LANCZOS4
+        )
+        gray_test_image = cv.cvtColor(test_image, cv.COLOR_RGB2GRAY)
+
+        corner_coordinates = harris.detect_harris_corners(gray_test_image)
+
+        for coordinate in corner_coordinates:
+            center = [int(coordinate[1]), int(coordinate[0])]
+            test_image = cv.circle(
+                test_image, center, radius=1, color=(255, 0, 0), thickness=-1
+            )
+
+        window_name = "window"
+        cv.namedWindow(
+            window_name, cv.WINDOW_NORMAL | cv.WINDOW_KEEPRATIO | cv.WINDOW_GUI_EXPANDED
+        )
+        cv.imshow(window_name, test_image)
+        cv.waitKey()
+
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(TestHarrisDetector("test_harris_visual"))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="structure_from_motion", version="0.1", packages=find_packages())

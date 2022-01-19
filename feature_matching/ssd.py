b485d3f0a5cba700ab4470458b01bbb8ea519c46
Istvan Balazs Opra <opra.balazs@gmail.com>
2022-01-19 20:12:48 +0100

    Finish implementing SSD

--- feature_matching/ssd.py
+++ feature_matching/ssd.py
@@ -1,4 +1,4 @@
-from typing import Sequence, Tuple
+from typing import Tuple
 
 import numpy as np
 
@@ -11,7 +11,25 @@ def calculate_ssd(
     feature_b: feat.Feature,
     window_size: int = 5,
 ) -> float:
-    pass
+    """Calculate the Squared Sum of Differences between two patches of an image.
+
+    :param image: The image where the features belong to.
+    :param feature_a: One feature identified by an image coordinate, center of one of the patches
+    :param feature_b: Center of the other patch
+    :param window_size: The size of a patch. If the patch would extend beyond the image size for either feature_a or
+    feature_b, np.Infinity is returned.
+    :return: The SSD between the two patches.
+    """
+    if not _is_within_bounds(
+        feature_a, image.shape, window_size
+    ) or not _is_within_bounds(feature_b, image.shape, window_size):
+        return np.Infinity
+
+    diff = _select_window(image, feature_a, window_size) - _select_window(
+        image, feature_b, window_size
+    )
+    square_diff = np.square(diff)
+    return np.sum(square_diff)
 
 
 def _is_within_bounds(
@@ -24,3 +42,13 @@ def _is_within_bounds(
         return False
 
     return True
+
+
+def _select_window(
+    image: np.ndarray, feature: feat.Feature, window_size: int
+) -> np.ndarray:
+    half_window_size = int(window_size / 2)
+    return image[
+        feature.y - half_window_size : feature.y + half_window_size + 1,
+        feature.x - half_window_size : feature.x + half_window_size + 1,
+    ]
--- feature_matching/tests/ssd_test.py
+++ feature_matching/tests/ssd_test.py
@@ -1,6 +1,8 @@
 from functools import partial
 import unittest
 
+import numpy as np
+
 from common import feature
 import feature_matching.ssd as ssd
 
@@ -20,3 +22,35 @@ class SsdTest(unittest.TestCase):
         self.assertTrue(is_within_bounds(feature.Feature(y=97, x=197)))
         self.assertFalse(is_within_bounds(feature.Feature(y=98, x=197)))
         self.assertFalse(is_within_bounds(feature.Feature(y=97, x=198)))
+
+    def test_ssd(self):
+        image = np.array(
+            [
+                [1, 2, 3, 0, 0, 0],
+                [4, 5, 6, 0, 0, 0],
+                [7, 8, 9, 0, 0, 0],
+                [0, 0, 0, 9, 8, 7],
+                [0, 0, 0, 6, 5, 4],
+                [0, 0, 0, 3, 2, 1],
+            ]
+        )
+
+        calculate_ssd = lambda feature_a, feature_b: ssd.calculate_ssd(
+            image, feature_a, feature_b, window_size=3
+        )
+
+        output = calculate_ssd(feature.Feature(1, 1), feature.Feature(4, 4))
+        sq = lambda x: x ** 2
+
+        self.assertEqual(
+            sq(8) + sq(6) + sq(4) + sq(2) + sq(0) + sq(2) + sq(4) + sq(6) + sq(8),
+            output,
+        )
+
+        # Check out-of-bounds features
+        self.assertEqual(
+            np.Infinity, calculate_ssd(feature.Feature(0, 0), feature.Feature(4, 4))
+        )
+        self.assertEqual(
+            np.Infinity, calculate_ssd(feature.Feature(1, 1), feature.Feature(5, 5))
+        )

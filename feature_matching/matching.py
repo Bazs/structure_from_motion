ff9147e4b4674c051f54a7da614feb07d6ef193c
Istvan Balazs Opra <opra.balazs@gmail.com>
2022-01-22 12:50:59 +0100

    Add test for brute force matcher, enable unittest discovery

--- feature_matching/matching.py
+++ feature_matching/matching.py
@@ -18,8 +18,15 @@ def match_features(
     features_a: List[Feature],
     features_b: List[Feature],
     error_function: ErrorFunction,
-    error_threshold: float,
 ) -> List[Match]:
+    """
+    Matches two list features using a brute-force pairwise method.
+
+    :param features_a: First list of features.
+    :param features_b: Second list of features.
+    :param error_function: An error function which returns the error of a particular match.
+    :return: List of matches for every feature in features_a, in the order of features_a.
+    """
     feature_a_matches = [Match() for _ in range(len(features_a))]
 
     for a_index, feature_a in enumerate(features_a):
@@ -29,10 +36,4 @@ def match_features(
                 feature_a_matches[a_index].best_match_error = error
                 feature_a_matches[a_index].best_match_index = b_index
 
-    matches = [
-        match
-        for match in feature_a_matches
-        if match.best_match_error <= error_threshold
-    ]
-
-    return matches
+    return feature_a_matches
---
+++ feature_matching/tests/test_matching.py
@@ -0,0 +1,34 @@
+import unittest
+
+from common.feature import Feature
+from feature_matching import matching
+
+
+class MatchingTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.features_a = [Feature(1, 1), Feature(2, 2), Feature(3, 3)]
+        self.features_b = [Feature(4, 4), Feature(5, 5), Feature(6, 6), Feature(7, 7)]
+
+    def _mock_matching_function(self, feature_a: Feature, feature_b: Feature):
+        # Dict of {feature_a_index: {if_matched_with_feature_b_index: error}}
+        feature_a_match_map = {
+            0: {0: 10, 1: 20, 2: 30, 3: 7},
+            1: {0: 30, 1: 9, 2: 20, 3: 15},
+            2: {0: 20, 1: 30, 2: 8, 3: 31},
+        }
+        possible_matches_for_a = feature_a_match_map[self.features_a.index(feature_a)]
+        score = possible_matches_for_a[self.features_b.index(feature_b)]
+        return score
+
+    def test_match_features(self):
+        matches = matching.match_features(
+            self.features_a,
+            self.features_b,
+            self._mock_matching_function,
+        )
+        expected_matches = [
+            matching.Match(best_match_index=3, best_match_error=7),
+            matching.Match(best_match_index=1, best_match_error=9),
+            matching.Match(best_match_index=2, best_match_error=8),
+        ]
+        self.assertEqual(expected_matches, matches)

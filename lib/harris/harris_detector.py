172cc0a27d68ef032e2eb375b001257b6510f95c
Istvan Balazs Opra <opra.balazs@gmail.com>
2022-01-26 20:52:11 +0100

    Add "apps" folder, move libraries to "libs", start with main pipeline

--- lib/harris/harris_detector.py
+++ lib/harris/harris_detector.py
@@ -8,7 +8,7 @@ _sobel_x_kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=float)
 
 
 def detect_harris_corners(
-        image: np.ndarray, num_corners: int = 50, block_size: int = 2, k: float = 0.04
+    image: np.ndarray, num_corners: int = 50, block_size: int = 2, k: float = 0.04
 ) -> List[feature.Feature]:
     """Detects corners in the image using the Harris corner detector algorithm.
     See https://en.wikipedia.org/wiki/Harris_corner_detector.
@@ -54,7 +54,7 @@ def detect_harris_corners(
 
 
 def _calculate_cornerness_image(
-        image: np.ndarray, block_size: int = 2, k: float = 0.04
+    image: np.ndarray, block_size: int = 2, k: float = 0.04
 ):
     sobel_y = _apply_sobel_y(image)
     sobel_x = _apply_sobel_x(image)
@@ -85,9 +85,9 @@ def _calculate_cornerness_image(
 
 
 def _select_window(
-        matrix: np.ndarray, row: int, col: int, window_size: int
+    matrix: np.ndarray, row: int, col: int, window_size: int
 ) -> np.ndarray:
-    return matrix[row: row + window_size, col: col + window_size]
+    return matrix[row : row + window_size, col : col + window_size]
 
 
 def _non_max_suppress(image: np.ndarray):

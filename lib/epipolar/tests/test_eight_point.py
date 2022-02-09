a68e4ffcf3efb500ce886d14a995603a9ecf9a28
Istvan Balazs Opra <opra.balazs@gmail.com>
2022-02-09 20:32:18 +0100

    Add first camera to eight-point test

--- lib/epipolar/tests/test_eight_point.py
+++ lib/epipolar/tests/test_eight_point.py
@@ -3,6 +3,7 @@ import unittest
 from scipy.spatial.transform import Rotation
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2.cv2 as cv
 
 from lib.common.feature import Feature
 from lib.epipolar import eight_point
@@ -71,30 +72,67 @@ class EightPointTest(unittest.TestCase):
         world_t_world_rectangleBOffset = np.array([2.0, 0.0, 0.0])
         world_t_world_rectangle_b += world_t_world_rectangleBOffset
 
-        rect_a_rot = Rotation.from_euler("y", 20.0, degrees=True)
-        world_t_world_rectangle_a = rect_a_rot.apply(world_t_world_rectangle_a)
+        rect_a_rot = Rotation.from_euler("y", -30.0, degrees=True)
+        world_t_world_rectangle_a = self._rotate_rectangle(
+            world_t_world_rectangle_a, rect_a_rot
+        )
 
-        rect_b_rot = Rotation.from_euler("y", -20.0, degrees=True)
-        world_t_world_rectangle_b = rect_b_rot.apply(world_t_world_rectangle_b)
+        rect_b_rot = Rotation.from_euler("y", 30.0, degrees=True)
+        world_t_world_rectangle_b = self._rotate_rectangle(
+            world_t_world_rectangle_b, rect_b_rot
+        )
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
         self._plot_rectangle(ax, world_t_world_rectangle_a)
         self._plot_rectangle(ax, world_t_world_rectangle_b)
+
+        world_t_world_camera_1 = np.array([1.5, 0.25, -1.0])
+        camera_R_world = np.eye(3, dtype=float)
+        camera_Rvec_world = cv.Rodrigues(camera_R_world)
+        (camera_Rvec_world)
+
+        self._plot_camera(ax, world_t_world_camera_1, camera_R_world)
+
         # plt.show()
-        # TODO define camera poses and do projection
 
     @staticmethod
-    def _rotate_rectangle(world_t_world_rectangle: np.ndarray, rotation: Rotation):
-        # TODO transform rectangle the world origin before rotation
-        pass
+    def _rotate_rectangle(
+        world_t_world_rectangle: np.ndarray, rotation: Rotation
+    ) -> np.ndarray:
+        centroid = np.mean(world_t_world_rectangle, axis=0)
+        rectangle_t_rectangle_rectangle = world_t_world_rectangle - centroid
+        rotated_rectangle = rotation.apply(rectangle_t_rectangle_rectangle)
+        return rotated_rectangle + centroid
 
     @staticmethod
-    def _plot_rectangle(ax: plt.axes, rectangle: np.ndarray):
+    def _plot_rectangle(ax: plt.axes, rectangle: np.ndarray) -> None:
         xs = np.append(rectangle[:, 0], rectangle[0, 0])
         ys = np.append(rectangle[:, 1], rectangle[0, 1])
         zs = np.append(rectangle[:, 2], rectangle[0, 2])
-        ax.plot(xs, ys, zs, zdir="y")
+        ax.plot(xs, ys, zs)
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+
+    @staticmethod
+    def _plot_camera(
+        ax: plt.axes, world_t_world_camera: np.ndarray, camera_R_world: np.ndarray
+    ) -> None:
+        ax.scatter(
+            world_t_world_camera[0], world_t_world_camera[1], world_t_world_camera[2]
+        )
+
+        arrow_length = 0.3
+        arrow_end = world_t_world_camera + camera_R_world.dot(
+            np.array([0.0, 0.0, arrow_length])
+        )
+
+        xs = np.array([world_t_world_camera[0], arrow_end[0]])
+        ys = np.array([world_t_world_camera[1], arrow_end[1]])
+        zs = np.array([world_t_world_camera[2], arrow_end[2]])
+
+        ax.plot(xs, ys, zs)
 
 
 if __name__ == "__main__":

Fix rotation extraction from essential matrix

lib/epipolar/eight_point.py
@@ -93,13 +93,26 @@ def recover_r_t(
     """
     u, s, vh = np.linalg.svd(e)
 
+    det_u = np.linalg.det(u)
+    det_vh = np.linalg.det(vh)
+    if not np.isclose(abs(det_u), 1):
+        raise EightPointCalculationError("U is not a rotation matrix")
+    if not np.isclose(abs(det_vh), 1):
+        raise EightPointCalculationError("V_h is not a rotation matrix")
+
+    # Make rotation matrix sub-components proper if they are improper
+    if np.isclose(-1, det_u):
+        u *= -1
+    if np.isclose(-1, det_vh):
+        vh *= -1
+
     if not np.isclose(0.0, s[-1]):
         raise EightPointCalculationError(
             "The smallest singular value of the Essential matrix is expected to be ~0"
         )
 
     w = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]], dtype=float)
-    z = np.array([[0, 1, 0], [-1, 0.0, 0.0], [0.0, 0.0, 0.0]], dtype=float)
+    z = np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 0]], dtype=float)
     t_x = u @ z @ u.T
     t_1 = np.array([-t_x[1, 2], t_x[0, 2], -t_x[0, 1]])
     R_1 = u @ w.T @ vh

lib/epipolar/tests/test_eight_point.py
@@ -167,9 +167,26 @@ class EightPointTest(unittest.TestCase):
         t_cv = np.squeeze(t_cv)
         R1, R2, t = eight_point.recover_r_t(features_1[0], features_2[0], e)
 
-        if not np.allclose(t_cv, t):
-            if not np.allclose(-t_cv, t):
+        def _allclose(expected: npt.NDArray, actual: npt.NDArray):
+            ABSOLUTE_TOLERANCE = 1e-4
+            return np.allclose(expected, actual, atol=ABSOLUTE_TOLERANCE)
+
+        if not _allclose(t_cv, t):
+            if not _allclose(-t_cv, t):
                 self.fail(f"Incorrect translation, expected:\n{t_cv}\nactual:\n{t}")
+
+        rotmat_comparison_failure_message = (
+            f"Incorrect estimates for rotation matrices. Expected:\n{R1_cv}\nand\n{R2_cv}\n"
+            f"Actual:\n{R1}\nand\n{R2}"
+        )
+        if _allclose(R1_cv, R1):
+            if not _allclose(R2_cv, R2):
+                self.fail(rotmat_comparison_failure_message)
+        elif _allclose(R2_cv, R1):
+            if not _allclose(R1_cv, R2):
+                self.fail(rotmat_comparison_failure_message)
+        else:
+            self.fail(rotmat_comparison_failure_message)
 
     def test_estimate_essential_matrix_degenerate(self):
         rectangle_width = np.array([1.0, 0.0, 0.0])